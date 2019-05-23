6a51f1d17180fcc737829a34dff9f49d02e16e59
Fifciuu <filip.jdrasik@gmail.com>
2019-05-23 12:07:04 +0200

    MVP Version

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ export default async function nuxtBootstrapVue (moduleOptions) {
   // Register core
   // It is under extensions, because this.addPlugin adds plugin at the beginning of the array
   this.addPlugin({
-    src: path.resolve(__dirname, '../plugins/nuxt-wp-json.js'),
+    src: path.resolve(__dirname, './plugins/nuxt-wp-json.js'),
     filename: 'wpjson.js',
     options: this.options.wpJson
   })
-
 
   const r = routes(true)
   this.nuxt.options.router.extendRoutes = (nuxtRoutes, resolve) => {
