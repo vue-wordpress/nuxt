6cf730cf1f3939c0c9fdba53ff5ce92938fa5bdd
Fifciuu <filip.jdrasik@gmail.com>
2019-05-23 12:47:50 +0200

    Support for both ways of provide config

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ export default async function nuxtBootstrapVue (moduleOptions) {
 
   // Register core
   // It is under extensions, because this.addPlugin adds plugin at the beginning of the array
+
   this.addPlugin({
     src: path.resolve(__dirname, './plugins/nuxt-wp-json.js'),
     filename: 'wpjson.js',
-    options: this.options.wpJson
+    options: (moduleOptions && Object.keys(moduleOptions).length > 0)
+      ? moduleOptions
+      : this.options.wpJson
   })
 
   const r = routes(true)
