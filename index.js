e10a4120f43cca93466077e98448d38a1e103798
Fifciuu <filip.jdrasik@gmail.com>
2019-06-04 18:19:52 +0200

    Dev can disable autoadding routes

--- index.js
+++ index.js
@@ -5,23 +5,26 @@ export default async function nuxtBootstrapVue (moduleOptions) {
 
   // Register core
   // It is under extensions, because this.addPlugin adds plugin at the beginning of the array
+  const config = (moduleOptions && Object.keys(moduleOptions).length > 0)
+    ? moduleOptions
+    : this.options.wpJson
 
   this.addPlugin({
     src: path.resolve(__dirname, './plugins/nuxt-wp-json.js'),
     filename: 'wpjson.js',
-    options: (moduleOptions && Object.keys(moduleOptions).length > 0)
-      ? moduleOptions
-      : this.options.wpJson
+    options: config
   })
 
-  const r = routes(true)
-  this.nuxt.options.router.extendRoutes = (nuxtRoutes, resolve) => {
-    nuxtRoutes.push(...r.map((route) => {
-
-      let cmp = route.component.replace('../', 'node_modules/@vue-wordpress/core/')
-      return { ...route, component: resolve(cmp) }
-
-    }))
+  if(config.router === true) {
+    const r = routes(true)
+    this.nuxt.options.router.extendRoutes = (nuxtRoutes, resolve) => {
+      nuxtRoutes.push(...r.map((route) => {
+  
+        let cmp = route.component.replace('../', 'node_modules/@vue-wordpress/core/')
+        return { ...route, component: resolve(cmp) }
+  
+      }))
+    }
   }
 
 }
